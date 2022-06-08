44f8807e94ef127b56e03074fbe005abe30731d2
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-06-07 23:56:46 -0400

    Not working

--- app2.py
+++ app2.py
@@ -1,7 +1,10 @@
 import os
+from pydoc import cli
 from slack_sdk import WebClient
 from flask import Flask, request
 from dotenv import load_dotenv
+from pathlib import Path
+
 
 # Initializing .env path
 env_path = Path('.') / '.env'
@@ -10,6 +13,10 @@ load_dotenv(dotenv_path=env_path)
 
 client_id = os.environ["SLACK_CLIENT_ID"]
 client_secret = os.environ["SLACK_CLIENT_SECRET"]
-oauth_scope = os.environ["SLACK_SCOPES"]
+# oauth_scope = os.environ["SLACK_SCOPES"]
+
+client = WebClient(token=os.environ["SLACK_TOKEN"])
+client.chat_postMessage(channel="#alpaca-slack-bot", text="Hello World!")
+
 
 app = Flask(__name__)
