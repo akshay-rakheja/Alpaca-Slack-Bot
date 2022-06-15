b5460fdb63620502574b1de827c2f67fdbadd868
garywuuu <garywu@college.harvard.edu>
2022-06-15 13:44:53 -0700

    buy/sell working

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 BASE_TOKEN_URL = "https://api.alpaca.markets/oauth/token"
 NGROK = "https://a272-152-44-181-213.ngrok.io"
-BASE_ALPACA_PAPER_URL = 'https://paper-api.alpaca.markets/v2/orders'
+BASE_ALPACA_PAPER_URL = 'https://paper-api.alpaca.markets'
 # BASE_ALPACA_URL = 'https://api.alpaca.markets'
    
 # Initializes your app with your bot token and signing secret
@@ -93,6 +93,71 @@ def buy():
         params = text.split()
         symbol = params[0]
         qty = params[1]
+        print(symbol + ' <---- this is the symbol')
+        print(qty + ' <---- this is the qty')
+        print(user_id + ' <---- this is the user id')
+    except:
+        print("Error getting data from slash command, perhaps missing/incorrect args")
+
+    # Get the access token from DB if the user_id exists
+    # TODO: error checking for user_id not in DB then redirect them to enter /alpaca command
+    try:
+        cur.execute(
+            'SELECT access_token FROM token WHERE user_id = %s', (user_id,))
+        access_token = cur.fetchone()[0]
+
+        print("Here's the access_token: ", access_token)
+        headers = {
+            'Authorization': 'Bearer ' + access_token
+        }
+        cur.close()
+        conn.close()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print("Error getting access token: ", error)
+
+    try:
+        order = requests.post(
+            '{0}/v2/orders'.format(BASE_ALPACA_PAPER_URL), headers=headers, json={
+                'symbol': symbol,
+                'qty': qty,
+                'side': 'buy',
+                'type': 'market',
+                'time_in_force': 'gtc'
+            })
+        print(order.json())
+    except Exception as e:
+        print("There was an issue posting order to Alpaca: {0}".format(e))
+
+    if order.status_code == 200 and order.json()['status'] == 'accepted':
+        return Response(f'Bought {qty} {symbol} on Alpaca!'), 200
+    else:
+        return Response("Error buying"), 200
+
+@app.route('/alpaca-sell', methods=['GET', 'POST'])
+def sell():
+    try:
+        # connect to db
+        conn = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME,
+                                user=config.DB_USER, password=config.DB_PASSWORD)
+        # Open Cursor
+        cur = conn.cursor()
+    except:
+        print("Error connecting to DB")
+
+    data = request.form
+
+    # Retrieve command args and verify user here
+    # TODO: error checking for bad args
+    try:
+        text = data['text']
+        lst = []
+        user_id = data['user_id']
+        params = text.split()
+        symbol = params[0]
+        qty = params[1]
+        print(symbol + ' <---- this is the symbol')
+        print(qty + ' <---- this is the qty')
+        print(user_id + ' <---- this is the user id')
     except:
         print("Error getting data from slash command, perhaps missing/incorrect args")
 
@@ -117,8 +182,8 @@ def buy():
         order = requests.post(
             '{0}/v2/orders'.format(BASE_ALPACA_PAPER_URL), headers=headers, json={
                 'symbol': symbol,
-                'qty': int(qty),
-                'side': 'buy',
+                'qty': qty,
+                'side': 'sell',
                 'type': 'market',
                 'time_in_force': 'gtc',
             })
@@ -126,19 +191,11 @@ def buy():
     except Exception as e:
         print("There was an issue posting order to Alpaca: {0}".format(e))
 
-    return Response("Buying"), 200
+    if order.status_code == 200 and order.json()['status'] == 'accepted':
+        return Response(f'Sold {qty} {symbol} on Alpaca!'), 200
+    else:
+        return Response("Error selling"), 200
 
-@app.route('/alpaca-sell', methods=['GET', 'POST'])
-def sell():
-    data = request.form
-    text = data['text']
-    lst = []
-    coms = text.split()
-    lst.append(coms)
-    
-    if lst.length != 2:
-        return Response("error"), 400
-    symbol = coms[0], qty = coms[1]
 
 def handleDisplayAccount(userID, token):
     data = request.form()
