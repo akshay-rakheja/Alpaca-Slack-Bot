e11b6aefa0a615a381a615840508fd5df7bdcd86
garywuuu <garywu@college.harvard.edu>
2022-06-14 20:00:20 -0700

    trying buy, db done

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 BASE_TOKEN_URL = "https://api.alpaca.markets/oauth/token"
 NGROK = "https://53b3-50-208-212-121.ngrok.io"
+BASE_ALPACA_URL = 'https://paper-api.alpaca.markets/v2/orders'
 # BASE_ALPACA_URL = 'https://api.alpaca.markets'
 # HEADERS = {'APCA-API-KEY-ID': os.environ['ALPACA_API_KEY'],
 #            'APCA-API-SECRET-KEY': os.environ['ALPACA_SECRET_KEY']}
@@ -41,16 +42,19 @@ def alpaca():
     if text == "connect":
         #client.chat_postEphemeral("https://api.alpaca.markets/oauth/grant_type=authorization_code&code=67f74f5a-a2cc-4ebd-88b4-22453fe07994&client_id=fc9c55efa3924f369d6c1148e668bbe8&client_secret=5b8027074d8ab434882c0806833e76508861c366&redirect_uri=https://example.com/oauth/callback")
         return Response("https://app.alpaca.markets/oauth/authorize?response_type=code&client_id=0c76f3a44caa688859359cab598c9969" + 
-        "&redirect_uri=" + NGROK + "/auth&scope=account:write%20trading%20data"), user_id
-    elif text == "display":
-        return Response(handleDisplayAccount(user_id, 0)), 200
+        "&redirect_uri=" + NGROK + "/auth&scope=account:write%20trading%20data&state=" + user_id), 200
+    # elif text == "display":
+    #     return Response(handleDisplayAccount(user_id, 0)), 200
     elif text == "":
         return Response("HI"), 200
 
 @app.route('/auth', methods=['GET', 'POST'])
-def auth(user_id):
-    dictionary = {}
+def auth():
+    conn = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME,
+                        user=config.DB_USER, password=config.DB_PASSWORD)
+    cur = conn.cursor()       
     auth_code = request.args.get("code")
+    user_id = request.args.get("state")
     print(auth_code + 'this is the auth code')
     redirect_uri = NGROK + '/auth'
     if auth_code != "":
@@ -60,13 +64,12 @@ def auth(user_id):
             'client_id': os.environ['ALPACA_CLIENT_ID'],
             'client_secret': os.environ['ALPACA_CLIENT_SECRET'],
             'redirect_uri': redirect_uri
-        }
+        })
         #set user's authentication status to true
-        )
     access_token = access_response.json()['access_token']
     print(access_token + ' <---- this is the access token')
     if access_token != "":
-        cur.execute('insert into token_table (user_id, access_token) values (%s, %s)', (
+        cur.execute('insert into token (user_id, access_token) values (%s, %s)', (
             user_id, access_token))
         conn.commit()
         cur.close()
@@ -89,11 +92,32 @@ def buy():
     # else statement - 
     # then error 
 
+     try:
+        order = requests.post(
+            '{0}/v2/orders'.format(BASE_ALPACA_URL), headers=HEADERS, json={
+                'symbol': symbol,
+                'qty': qty,
+                'side': "buy",
+                'type': "market",
+                'time_in_force': "gtc",
+            })
+        print('Alpaca order reply status code: {0}'.format(
+            order.status_code))
+        if order.status_code != 200:
+            print("Undesirable response from Alpaca! {}".format(order.json()))
+            return False
+    except Exception as e:
+        logger.exception(
+            "There was an issue posting order to Alpaca: {0}".format(e))
+        return False
+    return order.json()
+
 @app.route('/alpaca-sell', methods=['GET', 'POST'])
 def sell():
     data = request.form
     text = data['text'], lst = []
     coms = text.split(), lst.append(coms)
+    
     if lst.length() != 2:
         return Response("error"), 400
     symbol = coms[0], qty = coms[1]
